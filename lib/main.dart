3b6b1983405004a6bb5f2ccbcfe599a945418a49
Dolph Hincapie <dolph.hincapie@pragma.com.co>
2022-10-18 15:54:50 -0500

    Repository Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Repo Counter',
+      title: 'Repository Counter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
